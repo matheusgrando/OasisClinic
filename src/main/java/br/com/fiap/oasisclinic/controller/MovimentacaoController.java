827d1267f881ec5ce727c4d94912596d4a616b0f
Paolla Felix <127573307+paollafelixx@users.noreply.github.com>
2024-03-19 18:35:45 -0300

    bean

---
+++ src/main/java/br/com/fiap/oasisclinic/controller/MovimentacaoController.java
@@ -0,0 +1,31 @@
+package main.java.br.com.fiap.oasisclinic.controller;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.budgetbuddy.model.Movimentacao;
+import br.com.fiap.budgetbuddy.repository.MovimentacaoRepository;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("movimentacao")
+public class MovimentacaoController {
+
+    @Autowired
+    MovimentacaoRepository repository;
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public Movimentacao create(@RequestBody @Valid Movimentacao movimentacao){
+        return repository.save(movimentacao);
+    }
+    
+}
+
---
+++ src/main/java/br/com/fiap/oasisclinic/repository/MovimentacaoRepository.java
@@ -0,0 +1,7 @@
+package main.java.br.com.fiap.oasisclinic.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.budgetbuddy.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {}
